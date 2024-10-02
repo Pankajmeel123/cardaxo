e278bf13a1e7523b1b428c62117be6c6f0bb0a80
Pankaj Meel <pankaj.meel@sarvm.ai>
2024-10-02 11:25:43 +0530

    changes

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -24,7 +24,7 @@ export class TabsComponent implements OnInit {
     this.cardService.cardDetails({}).then((res:any)=>{
       if(res.status == 'ERROR' && res.message === 'Data Not Found'){
         this.userService.userKycDetail().then((res:any)=>{
-          if(res.status === 'OK' && res.data.user_kyc_details.user_kyc_status === "accepted"){
+          if(res.status === 'OK' && res.data?.user_kyc_details?.user_kyc_status === "accepted"){
             this.cardNav = 'cards/application';
             console.log(this.cardNav)
           }else{
